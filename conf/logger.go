5bf7e49d03772bccecfecfe0b8f19f61cfa1f774
hu417 <70016878+hu417@users.noreply.github.com>
2023-06-21 00:04:35 +0800

    Update logger.go

--- conf/logger.go
+++ conf/logger.go
@@ -13,6 +13,7 @@ import (
 
 func InitLogger() *zap.SugaredLogger {
 	logMode := zapcore.DebugLevel
+	// 定义日志级别 
 	if !viper.GetBool("mode.develop") {
 		logMode = zapcore.InfoLevel
 	}
@@ -20,10 +21,14 @@ func InitLogger() *zap.SugaredLogger {
 	return zap.New(core).Sugar()
 }
 
+// 定义输出格式
 func getEncoder() zapcore.Encoder {
 	encoderConfig := zap.NewProductionEncoderConfig()
+	// key值显示：time
 	encoderConfig.TimeKey = "time"
+	// 日志级别显示大写
 	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	// 时间格式化: YYYY-MM-DD HH-MM-SS; > 1.20版本
 	encoderConfig.EncodeTime = func(t time.Time, encoder zapcore.PrimitiveArrayEncoder) {
 		encoder.AppendString(t.Local().Format(time.DateTime))
 	}
@@ -31,12 +36,13 @@ func getEncoder() zapcore.Encoder {
 	return zapcore.NewJSONEncoder(encoderConfig)
 }
 
+// 定义输出
 func getWriteSyncer() zapcore.WriteSyncer {
 	stSeparator := string(filepath.Separator)
 	stRootDir, _ := os.Getwd()
-	stLogFilePath := stRootDir + stSeparator + "log" + stSeparator + time.Now().Format(time.DateOnly) + ".txt"
+	stLogFilePath := stRootDir + stSeparator + "log" + stSeparator + time.Now().Format(time.DateOnly) + ".log"
 	fmt.Println(stLogFilePath)
-
+        // 日志切割
 	luberjackSyncer := &lumberjack.Logger{
 		Filename:   stLogFilePath,
 		MaxSize:    viper.GetInt("log.MaxSize"),    // 日志文件最大的尺寸(M), 超限后开始自动分割
